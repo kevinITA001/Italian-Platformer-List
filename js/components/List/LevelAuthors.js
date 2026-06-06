5f839fa0d24983f86c7a1af846948b05c4f4cea0
kevinITA001 <kevincavallari8@gmail.com>
2026-06-06 15:14:14 +0200

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -8,7 +8,7 @@ export default {
             type: Array,
             required: true,
         },
-        verifier: {
+        first italy victor: {
             type: String,
             required: true,
         },
