6bc3229374059c321f8d1cbe349c6a03903021a7
kevinITA001 <kevincavallari8@gmail.com>
2026-06-06 14:48:39 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -98,7 +98,7 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h3>Submission Requirements</h3>
+                    <h3></h3>
                     <p>
                         Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
                     </p>
